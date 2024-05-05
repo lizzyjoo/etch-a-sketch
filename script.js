6561ad2d1ad54468b68fde148f0926fd5d57813b
lizzyjoo <jejviolinist@gmail.com>
2024-05-05 10:36:50 -0400

    refactor: rainbow mode added

--- script.js
+++ script.js
@@ -5,25 +5,54 @@ let output = document.getElementById('size-text');
 output.textContent = `${slider.value} x ${slider.value}`;
 let gridSize = slider.value;
 var board = createGrid();
+let colorSquare = '';
+let rainbowSquare = '';
+let shadingSquare = '';
+let randomColor = '';
 
-slider.oninput = function() {
-    output.textContent = `${this.value} x ${this.value}`;
-  }
 
-slider.onchange = function() {
-    gridSize = this.value;
+// user's choice of color (HEX value)
+let color = document.getElementById('favcolor').value;
 
-    let child = container.lastElementChild;
-    while (child) {
-        container.removeChild(child);
-        child = container.lastElementChild;
+// 1. Basic mode (single color)
+let basicBtn = document.getElementById('basic');
+basicBtn.addEventListener('click', colorBasic);
+// 2. Rainbow Mode (random color generated for each square)
+let rainbowBtn = document.getElementById('rainbow');
+rainbowBtn.addEventListener('click', rainbowPen);
+// 3. Shading mode
+let shadingBtn = document.getElementById('shading');
+shadingBtn.addEventListener('click', shadingPen);
+// 4. Clear the board
+let clearBtn = document.getElementById('clear-board');
+clearBtn.addEventListener('click', clearBoard);
+
+function sketchPad() {
+    colorBasic()
+
+    slider.oninput = function() {
+        output.textContent = `${this.value} x ${this.value}`;
+      }
+    
+    slider.onchange = function() {
+        gridSize = this.value;
+    
+        let child = container.lastElementChild;
+        while (child) {
+            container.removeChild(child);
+            child = container.lastElementChild;
+        }
+        board = createGrid();
+        colorMode(colorSquare,rainbowSquare,shadingSquare);
     }
-    console.log(this.value);
-    board = createGrid();
-    colorSquare();
+
+    // if the user changes color, need to update color
+    document.getElementById('favcolor').onchange = function() {
+        color = this.value;
+    }
+
     
 }
-
 
 function createRow() {
     const row = document.createElement('div');
@@ -52,9 +81,18 @@ function createGrid() {
         }
     } 
 }
-
-function colorSquare() {
-
+function colorMode(colorSquare, rainbowSquare, shadingSquare) {
+    if (colorSquare == 'True') {
+        colorBasic();
+    } else if (rainbowSquare == 'True') {
+        rainbowPen();
+    } else if (shadingSquare == 'True') {
+        //
+    }
+}
+function colorBasic() {
+    colorSquare = 'True';
+    rainbowSquare, shadingSquare = 'False';
     let squares = document.getElementsByName('square');
     var flag = false;
     window.onmouseup = () => {flag = false};
@@ -71,32 +109,49 @@ function colorSquare() {
 })
 
 }
-colorSquare()
 
-// UI
+function makeRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function rainbowPen() {
+    randomColor = makeRandomColor();
+    rainbowSquare = 'True';
+    colorSquare,shadingSquare = 'False';
+    let squares = document.getElementsByName('square');
+    var flag = false;
+    
+    window.onmouseup = () => {flag = false};
+    squares.forEach(square => {
+        square.onmouseover = () => {
+            if (flag) {
+                
+                square.style.backgroundColor = randomColor = makeRandomColor();
+            }
+        }
+        square.onmousedown = () => {
+            square.style.backgroundColor= randomColor = makeRandomColor();
+            flag = true;
+        }
+})
 
-// user's choice of color (HEX value)
-let color = document.getElementById('favcolor').value;
+}
 
-// if the user changes color, need to update color
-
-document.getElementById('favcolor').onchange = function() {
-    color = this.value;
-  }
-// button to clear 
-// rainbow 
-// shadow/opacity thingy 
+function shadingPen() {
+    
+}
 
 
-
-
-
+// clear the board
 function clearBoard() {
-    container.style.backgroundColor = 'white';
+    let squares = document.getElementsByName('square');
+    squares.forEach(square => {
+        square.style.backgroundColor = 'white';
+    })
 }
 
-function sketchBoard() {
-    // 1. not resized
-
-    // 2. resized
-}
+sketchPad()
